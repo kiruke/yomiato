1f6db269a3459acf15a0f03dddb1b7bdbcf1fec2
kiruke <kiruke2525@gmail.com>
2026-09-17 02:02:50 +0900

    Investigate Google Books API fetch

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :author, presence: true
-  validates :isbn, length: { is: 13 }
+  validates :isbn, length: { is: 13 }, allow_nil: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_12_131819) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_16_163812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "author"
     t.datetime "created_at", null: false
+    t.string "image_link"
+    t.string "info_link"
     t.string "isbn"
     t.date "published_at"
     t.string "title"
