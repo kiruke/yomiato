735e2378c1513554ca8cfd86ace1f907fd1a2de6
kiruke <kiruke2525@gmail.com>
2026-09-23 21:53:08 +0900

    Refactor Google Books API client

--- app/clients/google_books_client.rb
+++ app/clients/google_books_client.rb
@@ -1,10 +1,10 @@
+require "uri"
+require "open-uri"
 class GoogleBooksClient
-  require 'uri'
-  require "open-uri"
 
   def self.search(query)
-    query = URI.encode_www_form_component(query)
-    url = "https://www.googleapis.com/books/v1/volumes?q=#{query}"
+    encoded_query = URI.encode_www_form_component(query)
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{encoded_query}"
     url_with_key = "#{url}&key=#{ENV["GOOGLE_BOOKS_API_KEY"]}"
     response = URI.open(url_with_key)
     body = response.read
