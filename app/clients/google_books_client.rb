77a337466649cb23b4d83f22a770d52463590a9a
kiruke <kiruke2525@gmail.com>
2026-09-24 21:06:25 +0900

    Replace URI.open with Faraday

--- app/clients/google_books_client.rb
+++ app/clients/google_books_client.rb
@@ -1,13 +1,17 @@
-require "uri"
-require "open-uri"
 class GoogleBooksClient
+  BASE_URL = "https://www.googleapis.com/books/v1"
 
   def self.search(query)
-    encoded_query = URI.encode_www_form_component(query)
-    url = "https://www.googleapis.com/books/v1/volumes?q=#{encoded_query}"
-    url_with_key = "#{url}&key=#{ENV["GOOGLE_BOOKS_API_KEY"]}"
-    response = URI.open(url_with_key)
-    body = response.read
-    data = JSON.parse(body)
+    conn = Faraday.new(BASE_URL) do |f|
+      f.response :raise_error
+      f.options.open_timeout = 5
+      f.options.read_timeout = 10
+    end
+    response = conn.get("volumes") do |req|
+      req.params["q"] = query
+      req.params["key"] = ENV["GOOGLE_BOOKS_API_KEY"]
+    end
+    response_body = response.body
+    data = JSON.parse(response_body)
   end
 end
